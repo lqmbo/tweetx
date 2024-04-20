949ea1cac300dc9ca65d37be1d6b9771cb45faa7
lqmbo <ilians@protonmail.com>
2024-04-20 16:22:20 +0100

    Updated Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
 defineProps({
     canLogin: {
@@ -14,22 +15,27 @@ defineProps({
 <template>
 
     <Head title="Welcome" />
-    <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-        <Link v-if="$page.props.auth.user" :href="route('dashboard')"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-        Dashboard
+    <div class="flex items-center justify-between md:max-w-4xl md:m-auto px-2 md:px-2 py-2">
+        <Link>
+        <ApplicationLogo class="w-16 h-16" />
         </Link>
-
-        <template v-else>
-            <Link :href="route('login')"
+        <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-            Log in
+            Dashboard
             </Link>
 
-            <Link v-if="canRegister" :href="route('register')"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-            Register
-            </Link>
-        </template>
-    </nav>
+            <template v-else>
+                <Link :href="route('login')"
+                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                Log in
+                </Link>
+
+                <Link v-if="canRegister" :href="route('register')"
+                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                Register
+                </Link>
+            </template>
+        </nav>
+    </div>
 </template>
